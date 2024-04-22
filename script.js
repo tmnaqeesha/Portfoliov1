4aa0995137b07e8fee10dd4bb500ee72e1ffb87e
Tengku Maryam <147499240+tmnaqeesha@users.noreply.github.com>
2024-04-22 13:49:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+$('.slide-nav').on('click', function(e) {
+  e.preventDefault();
+  var current = $('.flex--active').data('slide'),
+    next = $(this).data('slide');
+
+  $('.slide-nav').removeClass('active');
+  $(this).addClass('active');
+
+  if (current === next) {
+    return false;
+  } else {
+    $('.slider__warpper').find('.flex__container[data-slide=' + next + ']').addClass('flex--preStart');
+    $('.flex--active').addClass('animate--end');
+    setTimeout(function() {
+      $('.flex--preStart').removeClass('animate--start flex--preStart').addClass('flex--active');
+      $('.animate--end').addClass('animate--start').removeClass('animate--end flex--active');
+    }, 800);
+  }
+});
